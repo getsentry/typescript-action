d61784b714ab14a61553dda233766db019cb00c8
Ryan Skonnord <ryan.skonnord@sentry.io>
2022-09-09 13:49:57 -0700

    Make main.js comply with new prettierrc

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import {wait} from './wait';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const ms: string = core.getInput('milliseconds');
+    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString());
     await wait(parseInt(ms, 10));
@@ -12,7 +12,7 @@ async function run(): Promise<void> {
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
