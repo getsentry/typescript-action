04a79eab6e81fdab6d2e33802d3485e55ee3ac07
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-03-20 15:37:58 -0700

    Re-syncing template with upstream

--- src/wait.ts
+++ src/wait.ts
@@ -1,4 +1,9 @@
-export async function wait(milliseconds: number): Promise<string> {
+/**
+ * Wait for a number of milliseconds.
+ * @param milliseconds The number of milliseconds to wait.
+ * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
+ */
+export function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
       throw new Error('milliseconds not a number');
